91a54a46f562b38fe6887d8f5bee5d0daa11a6ed
Oby <obyli214@gmail.com>
2023-01-09 23:04:17 -0800

    added realtime circle display and clear board

--- realtime-circles/io.js
+++ realtime-circles/io.js
@@ -3,6 +3,14 @@ var io = require('socket.io')();
 // Listen for new connections from clients (socket)
 io.on('connection', function(socket) {
     console.log('Client connected to socket.io');
+    // When the current client clicks or listens to add a circle
+    socket.on('add-circle', function(data) {
+        // The server receive the signal and sends it off to other clients
+        io.emit('add-circle', data)
+    })
+    socket.on('clear', function() {
+        io.emit('clear');
+    })
 });
 
 // io represents socket.io on the server - let's export it
--- realtime-circles/public/javascripts/main.js
+++ realtime-circles/public/javascripts/main.js
@@ -2,6 +2,16 @@
 const socket = io();
 console.log(socket);
 
+// listen to the server for the `add-circle` event
+socket.on('add-circle', function(data) {
+  console.log(data)
+  addCircle(data)
+})
+
+socket.on('clear', function() {
+  circles.innerHTML = '';
+})
+
 const circles = document.getElementById('circles');
 let initials = '';
 
@@ -11,10 +21,16 @@ do {
 
 // Listen for clicks anywhere in the "section"
 circles.addEventListener('click', function(evt) {
-  addCircle(evt.clientX, evt.clientY, randomBetween(20,125), getRandomRGBA());
+  socket.emit('add-circle', {
+    initials: initials,
+    x: evt.clientX,
+    y: evt.clientY,
+    dia: randomBetween(10, 100),
+    rgba: getRandomRGBA()
+  })
 });
 
-function addCircle(x, y, dia, rgba) {
+function addCircle({x, y, dia, rgba, initials}) {
   const el = document.createElement('div');
   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
@@ -26,7 +42,7 @@ function addCircle(x, y, dia, rgba) {
 }
 
 document.querySelector('button').addEventListener('click', function() {
-  circles.innerHTML = '';
+  socket.emit('clear')
 });
 
 function randomBetween(min, max) {
